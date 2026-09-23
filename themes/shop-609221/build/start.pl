b14aef13b2a7f56a451699a911c6283407286f4b
Reto S. <t7@algonetic.ch>
2026-09-23 17:20:06 +0200

    Further Development (ALGO)

--- themes/shop-609221/build/start.pl
+++ themes/shop-609221/build/start.pl
@@ -29,8 +29,10 @@ if ($type eq 'main') {
 	my \@products = ();
 	my %p1 = ( title => 't1', price => 9.55 );
 	my %p2 = ( title => 't2', price => 3.20 );
+	my %p3 = ( title => 't3', price => 4.20 );
 	push(\@products, \\%p1);
 	push(\@products, \\%p2);
+	push(\@products, \\%p3);
 
 	my %v = (
 		daily_609231 => 'Tagesangebote am Mittwoch',
