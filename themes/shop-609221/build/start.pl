e83082bda65de7e4a3c35666ce55ce3d9257f7c7
Reto S. <t7@algonetic.ch>
2026-09-23 16:25:38 +0200

    Further Development (ALGO)

--- themes/shop-609221/build/start.pl
+++ themes/shop-609221/build/start.pl
@@ -44,8 +44,8 @@ else {
 while(my $line = <IN>) {
 	chomp $line;
 	$line =~ s/\[%(.+)%\]/<%$1%>/g;
-	# XXX <& partials/header.html &>
-	$line =~ s/<& partials\/header.html &>/<& partials\/header.html, C => \$C &>/;
+	$line =~ s/<& partials\/(.+)\.html &>/<& partials\/$1\.html, C => \$C &>/;
+	# XX $line =~ s/<& partials\/header.html &>/<& partials\/header.html, C => \$C &>/;
 	print OUT "$line\n";
 }
 
