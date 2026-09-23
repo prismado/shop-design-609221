d76cee6db07326cb32fa13d29ecfa9ba2e451338
Reto S. <t7@algonetic.ch>
2026-09-23 07:18:24 +0200

    Further Development (ALGO)

--- themes/generate.pl
+++ themes/generate.pl
@@ -3,4 +3,20 @@ use strict;
 
 my $theme = $ARGV[0] // 'shop-609221';
 
-print "$theme\n";
+print "----- $theme -----\n";
+
+my $errstr = '';
+
+opendir(D, "$theme/public/start") or ($errstr = $!);
+
+if ($errstr) {
+	print STDERR "Error $errstr\n";
+	exit(1);
+}
+
+while(my $fn = readdir(D)) {
+	next if $fn =~ /^\./;
+	print "$fn\n";
+}
+
+closedir(D);
