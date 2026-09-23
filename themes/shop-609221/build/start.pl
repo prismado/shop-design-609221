7cd5639f4e5bbcb0d4d0d346cb7c06c0b18273a1
Reto S. <t7@algonetic.ch>
2026-09-23 07:13:41 +0200

    Check-in

--- themes/shop-609221/build/start.pl
+++ themes/shop-609221/build/start.pl
@@ -1,4 +1,17 @@
 #!/usr/bin/perl -w
 use strict;
 
-print 2+3;
+my $errstr = '';
+
+opendir(D, '../public/start') or ($errstr = $!);
+
+if ($errstr) {
+	print STDERR "Error $errstr\n";
+	exit(1);
+}
+
+while(my $fn = readdir(D)) {
+	print "$fn\n";
+}
+
+closedir(D);
